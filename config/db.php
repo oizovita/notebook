069c14ffe0b1528182da921ecb5c048b4b47d7c2
oizovita <oles1103@gmail.com>
2019-06-19 16:33:04 +0300

    update db

--- config/db.php
+++ config/db.php
@@ -1,10 +1,20 @@
 <?php
-
+$url = parse_url(getenv("DATABASE_URL"));
+$dsn = 'pgsql:host='.$url['host'].';port='.$url['port'].';dbname='.substr($url["path"], 1);
+$username = $url["user"];
+$password = $url["pass"];
 return [
+
+//    'class' => 'yii\db\Connection',
+//    'dsn' => "mysql:host=localhost;dbname=notebook",
+//    'username' => 'root',
+//    'password' => 'lesclaypool',
+//    'charset' => 'utf8',
+
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=notebook',
-    'username' => 'root',
-    'password' => 'lesclaypool',
+    'dsn' => $dsn,
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
